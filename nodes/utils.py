f1802801c2c0bba8fa2ea02b42944d0da0202f43
obotha <khyper234@gmail.com>
2019-05-21 16:14:21 +0300

    k

--- nodes/utils.py
+++ nodes/utils.py
@@ -1,7 +1,7 @@
 
 def analyseSeconds(gap,list_of_times, sID, table):
   #for @ station, @ node ------
-  var counter = 0
+  counter = 0
   while counter < len(list_of_times) - 1:
     print(list_of_times[counter]['time_in_seconds'])
     counter = counter + 1
