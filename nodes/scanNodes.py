029cc1fafc6703e4d1469e6bfc0a2bc9e2e417e7
obotha <khyper234@gmail.com>
2019-05-21 09:57:00 +0300

    l

--- nodes/scanNodes.py
+++ nodes/scanNodes.py
@@ -12,5 +12,6 @@ def scanNodes(stationsIDs):
         for table in list_of_tables:
             IDresult  = retrieveBiggestIdFromTable(sID[0],table)
             latest_secondsEpoch = IDresult[0][1]
+            latest_secondsEpoch = datetime.datetime.strptime(latest_secondsEpoch, '%Y-%m-%d,%H:%M:%S')
             print(sID[0], table, IDresult, latest_secondsEpoch)
 
