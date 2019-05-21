9226eb921bab240f50a4bd1863f64a7e32cbbd6b
obotha <khyper234@gmail.com>
2019-05-21 10:07:26 +0300

    a

--- nodes/scanNodes.py
+++ nodes/scanNodes.py
@@ -5,13 +5,16 @@
 from database.retrieveQuery import retrieveBiggestIdFromTable
 import datetime
 
-def scanNodes(stationsIDs):
-    list_of_tables = ['GroundNode', 'SinkNode', 'TenMeterNode', 'TwoMeterNode']
-    for sID in stationsIDs:
-        # sID[0]
-        for table in list_of_tables:
-            IDresult  = retrieveBiggestIdFromTable(sID[0],table)
-            latest_secondsEpoch = IDresult[0][1]
-            latest_secondsEpoch = datetime.datetime.strptime(latest_secondsEpoch, '%Y-%m-%d,%H:%M:%S')
-            print(sID[0], table, IDresult, latest_secondsEpoch)
 
+def scanNodes(stationsIDs):
+  list_of_tables = ['GroundNode', 'SinkNode', 'TenMeterNode', 'TwoMeterNode']
+  for sID in stationsIDs:
+    # sID[0]
+    for table in list_of_tables:
+      IDresult = retrieveBiggestIdFromTable(sID[0], table)
+      latest_secondsEpoch = IDresult[0][1]
+      if len('2019-02-21,17:53:16') == 19
+        latest_secondsEpoch = datetime.datetime.strptime(latest_secondsEpoch, '%Y-%m-%d,%H:%M:%S')
+      else:
+        latest_secondsEpoch = 0
+      print(sID[0], table, IDresult, latest_secondsEpoch)
