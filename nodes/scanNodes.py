eeeeea8cac3db45095da44d1eb0d7231e74c6d1a
obotha <khyper234@gmail.com>
2019-05-21 15:28:39 +0300

    if for an empty list

--- nodes/scanNodes.py
+++ nodes/scanNodes.py
@@ -16,22 +16,23 @@ def scanNodes(stationsIDs):
     for table in list_of_tables:
       IDresult = retrieveBiggestIdFromTable(sID[0], table)
       print(IDresult)
-      latest_secondsEpoch = IDresult[0][1]
-      if latest_secondsEpoch is not None and len(latest_secondsEpoch) == 19:
-        latest_secondsEpoch = datetime.datetime.strptime(latest_secondsEpoch, '%Y-%m-%d,%H:%M:%S')
-        latest_secondsEpoch = latest_secondsEpoch.timestamp()
-        now = datetime.datetime.now().timestamp()
-        gap = now - latest_secondsEpoch
-      else:
-        # corrupt data detected, no problem will be reported
-        latest_secondsEpoch = 0
-        second_latest_secondsEpoch = 0
-        gap = 'not calculated, corrupt data'
+      if len(IDresult) in not 0: 
+        latest_secondsEpoch = IDresult[0][1]
+        if latest_secondsEpoch is not None and len(latest_secondsEpoch) == 19:
+          latest_secondsEpoch = datetime.datetime.strptime(latest_secondsEpoch, '%Y-%m-%d,%H:%M:%S')
+          latest_secondsEpoch = latest_secondsEpoch.timestamp()
+          now = datetime.datetime.now().timestamp()
+          gap = now - latest_secondsEpoch
+        else:
+          # corrupt data detected, no problem will be reported
+          latest_secondsEpoch = 0
+          second_latest_secondsEpoch = 0
+          gap = 'not calculated, corrupt data'
 
-      result_for_trend = retrieveRTCforTrend(sID[0], table, IDresult[0][0])
-      #print(result_for_trend)
-      list_of_times = []
-      for rtc in result_for_trend:
-        if rtc[0] is not None and len(rtc[0]) == 19:
-          time = datetime.datetime.strptime(rtc[0], '%Y-%m-%d,%H:%M:%S')
-          list_of_times.append(time.timestamp())
+        result_for_trend = retrieveRTCforTrend(sID[0], table, IDresult[0][0])
+        #print(result_for_trend)
+        list_of_times = []
+        for rtc in result_for_trend:
+          if rtc[0] is not None and len(rtc[0]) == 19:
+            time = datetime.datetime.strptime(rtc[0], '%Y-%m-%d,%H:%M:%S')
+            list_of_times.append(time.timestamp())
