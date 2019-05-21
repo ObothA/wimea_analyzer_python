ad8c471c7a4b1b404e1cefabd4221e90b0f97443
obotha <khyper234@gmail.com>
2019-05-21 16:25:56 +0300

    a

--- nodes/utils.py
+++ nodes/utils.py
@@ -5,5 +5,5 @@ def analyseSeconds(gap,list_of_times, sID, table):
   while counter < len(list_of_times) - 1:
     difference = list_of_times[counter + 1]['time_in_seconds'] - list_of_times[counter]['time_in_seconds']
     difference = round(difference)
-    print(difference)
+    print(difference, list_of_times[counter + 1]['rtc'], list_of_times[counter]['rtc'])
     counter = counter + 1
