beaa30451895d49a8f41ac56c4c7232ccd3b2353
obotha <khyper234@gmail.com>
2019-05-21 18:46:11 +0300

    x

--- stations/stations.py
+++ stations/stations.py
@@ -8,7 +8,7 @@ from nodes.scanNodes import scanNodes
 def stations():
   #scan stations for on/off status
 
-  sql = "SELECT `station_id` FROM `stations` WHERE `station_id` > 49" # > 49, #50
+  sql = "SELECT `station_id` FROM `stations` WHERE `station_id` = 50" # > 49, #50
   stations_id_result = retrieveQuery(sql)
   result = stations_id_result[0]
 
