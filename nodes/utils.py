0769d905b047b23b8f89cc5552f7b4d9e68d0999
obotha <khyper234@gmail.com>
2019-05-21 17:22:51 +0300

    Subtract in descending order

--- nodes/utils.py
+++ nodes/utils.py
@@ -3,7 +3,7 @@ def analyseSeconds(gap,list_of_times, sID, table):
   #for @ station, @ node ------
   counter = 0
   while counter < len(list_of_times) - 1:
-    difference = list_of_times[counter + 1]['time_in_seconds'] - list_of_times[counter]['time_in_seconds']
+    difference = list_of_times[counter]['time_in_seconds'] - list_of_times[counter + 1]['time_in_seconds']
     difference = round(difference)
-    print(difference, list_of_times[counter + 1]['rtc'], list_of_times[counter]['rtc'])
+    print(difference, list_of_times[counter]['rtc'], list_of_times[counter + 1]['rtc'])
     counter = counter + 1
