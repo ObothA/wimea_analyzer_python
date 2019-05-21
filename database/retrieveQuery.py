c4be62d2d454065fe5818665e7b41378683322c9
obotha <khyper234@gmail.com>
2019-05-21 09:11:08 +0300

    Picking biggest id

--- database/retrieveQuery.py
+++ database/retrieveQuery.py
@@ -11,3 +11,12 @@ def retrieveQuery(sql):
   # finally:
   # connection.close()
   return [result]
+
+def retrieveBiggestIdFromTable(stationID,table):
+  ############################################
+  cursor = mydb.cursor()
+  sqlStatement = "select id, RTC_T from " +table+ " where stationID = " +stationID+ " "
+  cursor.execute(sqlStatement)
+  result = cursor.fetchall()
+  return result
+
--- nodes/scanNodes.py
+++ nodes/scanNodes.py
@@ -2,6 +2,13 @@
 #
 #
 
+from database.retrieveQuery import retrieveBiggestIdFromTable
+
 def scanNodes(stationsIDs):
+    list_of_tables = ['GroundNode', 'SinkNode', 'TenMeterNode', 'TwoMeterNode']
     for sID in stationsIDs:
-        print(sID[0])
+        # sID[0]
+        for table in list_of_tables:
+            IDresult  = retrieveBiggestIdFromTable(sID[0],table)
+            print(IDresult)
+
