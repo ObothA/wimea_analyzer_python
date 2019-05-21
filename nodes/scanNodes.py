18887576a29044843298d5f0ec14611ec1daf319
obotha <khyper234@gmail.com>
2019-05-21 10:16:14 +0300

    get timestamp

--- nodes/scanNodes.py
+++ nodes/scanNodes.py
@@ -15,6 +15,7 @@ def scanNodes(stationsIDs):
       latest_secondsEpoch = IDresult[0][1]
       if len(latest_secondsEpoch) == 19:
         latest_secondsEpoch = datetime.datetime.strptime(latest_secondsEpoch, '%Y-%m-%d,%H:%M:%S')
+        latest_secondsEpoch = latest_secondsEpoch.timestamp()
       else:
         latest_secondsEpoch = 0
       print(sID[0], table, IDresult, latest_secondsEpoch)
