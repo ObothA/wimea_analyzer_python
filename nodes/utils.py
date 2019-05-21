efc6203dd7b28721a9c72f0c544062d240d8c8ab
obotha <khyper234@gmail.com>
2019-05-21 18:07:44 +0300

    a

--- nodes/utils.py
+++ nodes/utils.py
@@ -2,14 +2,19 @@
 def analyseSeconds(gap,list_of_times, sID, table):
   #for @ station, @ node ------
   def sortFunc(object):
+    # function to sort dictionaries based on seconds
     return object['time_in_seconds']
 
   #sort to avoid negatives during subtraction
   list_of_times.sort(reverse=True, key=sortFunc)
 
+  clusters = []
   counter = 0
   while counter < len(list_of_times) - 1:
     difference = list_of_times[counter]['time_in_seconds'] - list_of_times[counter + 1]['time_in_seconds']
     difference = round(difference)
-    print(difference, list_of_times[counter]['rtc'], list_of_times[counter + 1]['rtc'])
+    #print(difference, list_of_times[counter]['rtc'], list_of_times[counter + 1]['rtc'])
+
+    for cluster in clusters:
+      print(cluster)
     counter = counter + 1
