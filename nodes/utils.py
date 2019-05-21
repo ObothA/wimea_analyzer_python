888e740c49f49161572f788375a628816d84518f
obotha <khyper234@gmail.com>
2019-05-21 17:31:17 +0300

    Sort to avoid negatives during subtraction

--- nodes/utils.py
+++ nodes/utils.py
@@ -1,6 +1,12 @@
 
 def analyseSeconds(gap,list_of_times, sID, table):
   #for @ station, @ node ------
+  def sortFunc(object):
+    return object['time_in_seconds']
+
+  #sort to avoid negatives during subtraction
+  list_of_times.sort(key=sortFunc)
+
   counter = 0
   while counter < len(list_of_times) - 1:
     difference = list_of_times[counter]['time_in_seconds'] - list_of_times[counter + 1]['time_in_seconds']
