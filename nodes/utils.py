2e62c07154fab11f9bd348e3be2846bc28564d16
obotha <khyper234@gmail.com>
2019-05-22 12:52:10 +0300

    a

--- nodes/utils.py
+++ nodes/utils.py
@@ -13,16 +13,18 @@ def analyseSeconds(gap,list_of_times, sID, table):
   counter = 0
   while counter < len(list_of_times) - 1:
     difference = list_of_times[counter]['time_in_seconds'] - list_of_times[counter + 1]['time_in_seconds']
+    difference = difference / 60
     difference = round(difference)
     #print(difference, list_of_times[counter]['rtc'], list_of_times[counter + 1]['rtc'])
 
-    append = True
-    for cluster in clusters:
-      if difference == cluster[0]:
-        cluster[1]  = cluster[1]+ 1
-        append = False
-    if append:
-        clusters.append([difference,1])
+    if difference > 1:
+      append = True
+      for cluster in clusters:
+        if difference == cluster[0]:
+          cluster[1]  = cluster[1]+ 1
+          append = False
+      if append:
+          clusters.append([difference,1])
     counter = counter + 1
   
   # sort the clusters
