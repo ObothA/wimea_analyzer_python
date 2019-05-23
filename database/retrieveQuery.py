2518dacbd1214d5a78a4049c66801cd01e462bf0
obotha <khyper234@gmail.com>
2019-05-23 18:15:24 +0300

    l

--- database/retrieveQuery.py
+++ database/retrieveQuery.py
@@ -72,7 +72,7 @@ def updateProblem(status, entryID):
 ######3
 def insertIntoChangeTracker(stationID, time_of_last_running_analyzer, Node, change, time_range_when_change_occured):
   cursor = mydb.cursor()
-  sql = "INSERT INTO ChangeTracker (stationID, time_of_last_running_analyzer, Node, change(minutes), time_range_when_change_occured) VALUES (%s, %s, %s, %s, %s)"
+  sql = "INSERT INTO ChangeTracker (stationID, time_of_last_running_analyzer, Node, change_in_minutes, time_range_when_change_occured) VALUES (%s, %s, %s, %s, %s)"
   time = datetime.datetime.now()
   val = (str(stationID),  time_of_last_running_analyzer, Node, change, time_range_when_change_occured)
   cursor.execute(sql, val)
